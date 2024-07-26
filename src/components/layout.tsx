2a22707c2736790862f5bef2ed82cd6220b6949f
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2024-07-26 11:55:26 +0900

    fix style

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,4 +1,4 @@
-import { Link, Outlet, useNavigate } from "react-router-dom";
+import { Link, Outlet, useLocation, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { auth } from "../firebase";
 import { IconHome, IconLogOut, IconProfile } from "../assets/Icons";
@@ -19,29 +19,28 @@ const Menu = styled.div`
   gap: 20px;
 `;
 
-const MenuItem = styled.div`
+const MenuItem = styled.div<{ active?: boolean }>`
   cursor: pointer;
   display: flex;
   align-items: center;
   justify-content: center;
-  border: 2px solid white;
+  border: 2px solid ${(props) => (props.active ? " #ab5bcb" : "white")};
+  opacity: ${(props) => (props.active ? 1 : 0.7)};
   height: 50px;
   width: 50px;
   border-radius: 50%;
   svg {
     width: 30px;
-    fill: white;
+    fill: ${(props) => (props.active ? " #ab5bcb" : "white")};
   }
-  &.log-out {
-    border-color: #9d47ff;
-    svg {
-      fill: #9d47ff;
-    }
+  &:hover {
+    opacity: 1;
   }
 `;
 
 function LayOut() {
   const navigate = useNavigate();
+  const currentPage = useLocation().pathname;
   const onLogOut = async () => {
     const ok = confirm("ログアウトします。よろしいですか?");
     if (ok) {
@@ -54,13 +53,13 @@ function LayOut() {
     <Wrapper>
       <Menu>
         <Link to="/">
-          <MenuItem>
+          <MenuItem active={currentPage === "/"}>
             <IconHome />
           </MenuItem>
         </Link>
 
         <Link to="/profile">
-          <MenuItem>
+          <MenuItem active={currentPage === "/profile"}>
             <IconProfile />
           </MenuItem>
         </Link>
